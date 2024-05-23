3601fd67e8e561827835e1365b1a06c3fb871b38
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-23 07:05:38 +0600

    add new functions and elements

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let textarea = window.document.querySelector('textarea');
 let mainScreen = window.document.querySelector('#main-screen');
 let editScreen = window.document.querySelector('#edit-screen');
 let notesCount = window.document.querySelector('#notes-count');
+let tableBody = window.document.querySelector('tbody');
 
 //Переменные
 
@@ -11,13 +12,52 @@ let noteId = -1;
 
 //Функций
 
+const createNewTableRow = () => {
+//     <tr>
+//     <td class="notes-title">
+//         Название
+//         <br>
+//         <span class="note-date">May 22 , 2024</span>
+//     </td>
+//     <td>
+//         <i class="material-icons note-del-icon">
+//             -
+//         </i>
+//     </td>
+// </tr>
+
+    //Создание тегов
+    let row = window.document.createElement('tr');
+    let titleColumn = window.document.createElement('td');
+    let delColumn = window.document.createElement('td');
+    let delIcon = window.document.createElement('i');
+    let date = window.document.createElement('span');
+    //Установка классов
+    titleColumn.className = 'note-title';
+    date.className = 'note-date';
+    delIcon.className = 'material-icons note-del-icon';
+    //Устанавливаем содержимое 
+    date.innerText = noteDate;
+    delIcon.innerText = 'delete_outlined';
+    titleColumn.innerText = noteTitle;
+     // Добавление элементов
+     delColumn.appendChild(delIcon);
+     titleColumn.appendChild(window.document.createElement('br'));
+     titleColumn.appendChild(date);
+     row.appendChild(titleColumn);
+     row.appendChild(delColumn);
+     tableBody.appendChild(row);
+}
+
 const onLoad = () => {
     noteList = window.localStorage.getItem('noteList')
     ? JSON.parse(window.localStorage.getItem('noteList'))
     : [];
     notesCount.innerText = `Заметок ${noteList.length}`;
     if(noteList) {
-        noteList.map(elem => console.log(elem));
+        noteList.map(elem => {
+            createNewTableRow(elem.id, elem.title, elem.date);
+        });
     }
 }
 
