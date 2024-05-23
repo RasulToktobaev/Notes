9be3c3ec5cf320f99546d79c606ce23fdf10e26a
RasulToktobaev <toktobaevrasul2002@bk.ru>
2024-05-23 06:30:08 +0600

    create function for back

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let input = window.document.querySelector('input');
+let textarea = window.document.querySelector('textarea');
+
+
+//Переменные
+
+
 const createNewNote = () => {
-    let date = new Date()
+    let date = moment(new Date());
+    let object = {
+        id:0,
+        title:input.value,
+        text:textarea.value,
+        date:date.format('LL')
+    }
+    console.log(object);
 }
+
+//Обработчики событий 
+ const backIconClick = () => {
+    createNewNote();
+ }
